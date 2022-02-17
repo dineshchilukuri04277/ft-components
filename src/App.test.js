8807630191d46990855ef39243774ac8cddf0460
dineshchilukuri04277 <dineshchilukuri04277@gmail.com>
2022-02-17 11:45:29 +0530

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import SharedComponent from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<SharedComponent />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/components/firstCoponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FirstComponent =({name}) => {
+    return <div>my name is {name}</div>
+}
+
+export default FirstComponent;
