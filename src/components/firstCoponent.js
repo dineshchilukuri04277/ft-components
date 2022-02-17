72366a009aa03b5b22dca63e9d72f1bee708813b
dineshchilukuri04277 <dineshchilukuri04277@gmail.com>
2022-02-17 13:04:33 +0530

    added function

--- src/components/firstCoponent.js
+++ src/components/firstCoponent.js
@@ -1,6 +1,11 @@
 import React from "react";
 
+export const printName = (name) => {
+    return `my name is ${name}`;
+}
+
 const FirstComponent =({name}) => {
+    
     return <div>my name is {name}</div>
 }
 
